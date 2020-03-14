d04abf8606defaef8b87387c38a1e1f37d9c89e2
HenrikAndreas <henrikbratli@live.no>
2020-03-14 15:45:08 +0100

    Finished home layout

--- website/main.py
+++ website/main.py
@@ -6,13 +6,13 @@ posts = [
         'title' : 'Welcome',
         'author' : 'Henrik Andreas',
         'date' : '16.12.2019',
-        'content' : 'Welcome to my webpage, which is full of bullshit. On the playground you might be able to have some fun'
+        'content' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
    {
         'title' : 'Great title',
         'author' : 'Henrik Andreas',
         'date' : '16.12.2019',
-        'content' : 'Welcome to my webpage, which is full of bullshit. On the playground you might be able to have some fun'
+        'content' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }
     
 ]
