a8f170f23921a30a397a5a819bc210e1a607e9b8
HenrikAndreas <henrikbratli@live.no>
2020-03-14 13:07:08 +0100

    fixed better navbar with bootstrap

--- website/main.py
+++ website/main.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 
+
+posts = [
+    {
+        'title' : 'Welcome',
+        'author' : 'Henrik Andreas',
+        'date' : '16.12.2019',
+        'content' : 'Welcome to my webpage, which is full of bullshit. On the playground you might be able to have some fun'
+    }
+]
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def mainScreen():
-    return render_template('layout.html')
+    return render_template('home.html', post=posts)
 
 
 if __name__ == '__main__':
