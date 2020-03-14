2a3ffaff10e33010532fc349720935e861620195
HenrikAndreas <henrikbratli@live.no>
2020-03-14 14:59:52 +0100

    Completed home / main page layout

--- website/main.py
+++ website/main.py
@@ -7,14 +7,21 @@ posts = [
         'author' : 'Henrik Andreas',
         'date' : '16.12.2019',
         'content' : 'Welcome to my webpage, which is full of bullshit. On the playground you might be able to have some fun'
+    },
+   {
+        'title' : 'Great title',
+        'author' : 'Henrik Andreas',
+        'date' : '16.12.2019',
+        'content' : 'Welcome to my webpage, which is full of bullshit. On the playground you might be able to have some fun'
     }
+    
 ]
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def mainScreen():
-    return render_template('home.html', post=posts)
+    return render_template('home.html', posts=posts)
 
 
 if __name__ == '__main__':
